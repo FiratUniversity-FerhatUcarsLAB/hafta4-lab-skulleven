a535f5bee9c2d02dade290c0f46628f7f27f05ad
Skulleven <skulydon@outlook.com>
2025-10-24 11:42:05 +0300

    task-2 done

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -4,5 +4,41 @@
  * Tarih: 24.10.2025
  * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
  * 
- * Bu program kullanıcıdan 
+ * Bu program kullanıcıdan yarı çapı alıp dairenin alanı, çevresi ve çapını; aynı yarı çaplı kürenin hacmini ve yüzey alanını bulur.
  */
+
+import java.util.Scanner;
+
+public class GeometrikHesap{
+    public static void main(String[] args) {
+        System.out.println("=== GEOMETRIK HESAPLAYICI ===");
+
+        // Scanner objesini oluşturur.
+        Scanner input = new Scanner(System.in);
+        
+        // Pi'yi 3.14159 olarak belirler.
+        final double pi = 3.14159;
+
+        // Yarıçapı kullanıcından alır.
+        System.out.print("Yaricapi giriniz : ");
+        double yaricap = input.nextDouble();
+
+        // Gerekli hesaplamaları yapar
+        double dairealani = pi * Math.pow(yaricap, 2);
+        double dairenincevresi = 2 * pi * yaricap;
+        double dairenincapi = 2 * yaricap;
+        double kureninhacmi = (4.0/3.0) * pi * Math.pow(yaricap, 3);
+        double kureninalani = 4 * pi * Math.pow(yaricap, 2);
+
+        // Sonuçları yazdırır.
+        System.out.println("======SONUÇLAR======");
+        System.out.printf("Dairenin alani:          %.2f \n", dairealani);
+        System.out.printf("Dairenin cevresi:        %.2f \n", dairenincevresi);
+        System.out.printf("Dairenin capi:           %.2f \n", dairenincapi);
+        System.out.printf("Kurenin hacmi:           %.2f \n", kureninhacmi);
+        System.out.printf("Kurenin yuzey alani :    %.2f \n", kureninalani);
+
+        // Scanner'ı kapat
+        input.close();
+    }
+}
