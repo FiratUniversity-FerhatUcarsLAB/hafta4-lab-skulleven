6e0039d3de3eada2692c2a778a90ddacd14a79e3
Skulleven <skulydon@outlook.com>
2025-10-24 11:28:24 +0300

    task-1 done

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,1 +1,8 @@
-
+/*
+ * Ad Soyad: Melik Buğra Kara
+ * Ogrenci No: 250541110
+ * Tarih: 24.10.2025
+ * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
+ * 
+ * Bu program kullanıcıdan 
+ */
--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: Melik Buğra Kara
+ * Ogrenci No: 250541110
+ * Tarih: 24.10.2025
  * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
  * 
  * Bu program kullanicidan ogrenci bilgilerini alir ve
@@ -15,11 +15,12 @@ public class OgrenciBilgi {
     public static void main(String[] args) {
         // Scanner objesi olusturun
         
+        Scanner input = new Scanner(System.in);
         
         // Degisken tanimlamalari
-        // String ad, soyad;
-        // int ogrenciNo, yas;
-        // double gpa;
+        String ad, soyad;
+        int ogrenciNo, yas;
+        double gpa;
         
         
         // Kullanicidan bilgileri alin
@@ -27,34 +28,35 @@ public class OgrenciBilgi {
         System.out.println();
         
         // Ad
-        
+        System.out.print("Adinizi girin : ");
+        ad = input.next();
         
         // Soyad
-        
+        System.out.print("Soyadinizi girin : ");
+        soyad = input.next();
         
         // Ogrenci No
-        
+        System.out.print("Ogrenci No. girin : ");
+        ogrenciNo = input.nextInt();
         
         // Yas
-        
-        
+        System.out.print("Yasinizi girin : ");
+        yas = input.nextInt();
+
         // GPA
-        
+        System.out.print("GPA'inizi girin : ");
+        gpa = input.nextDouble();
         
         // Bilgileri ekrana yazdirin
         System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
         
         // COZUMUNUZU BURAYA YAZIN
-        
-        
-        
-        
-        
-        
-
-        
+        System.out.printf("Ad-Soyad: %s %s \n", ad, soyad);
+        System.out.printf("Ogrenci No : %d \n", ogrenciNo);
+        System.out.printf("Yas : %d \n", yas);
+        System.out.printf("GPA : %.2f \n", gpa);
         
         // Scanner'i kapatin (önemli pratik)
-        
+        input.close();
     }
 }
